85e1ec0a985c0e1e7ecca8c7b4099b4871c52233
itw_yinjm <540959120@qq.com>
2018-04-16 14:18:30 +0800

    -swipper广告滑动块添加

--- pages/sell/index.js
+++ pages/sell/index.js
@@ -24,46 +24,52 @@ Page({
     interval: 3000,
     duration: 1200,
     toView: 'blue',
-      //菜单
-      menus: null,
+    //菜单
+    menus: null,
+    advertisements: null,
     smallCategories: [],
-      menuIndex: 0,
-      //购物车
-      cart: [],
-      total: {
-          count: 0,
-          money: 0.0
-      },
+    menuIndex: 0,
+    //购物车
+    cart: [],
+    total: {
+      count: 0,
+      money: 0.0
+    },
     //地图
     markers: [],
     latitude: '',
     longitude: '',
     rgcData: {},
-      address: '',
-      //是否显示加载更多
-      isHideLoadMore: true
+    address: '',
+    //是否显示加载更多
+    isHideLoadMore: true,
+    //swipper 属性
+    indicatorDots: true, //是否显示面板指示点
+    autoplay: true, //是否自动切换
+    interval: 3000, //自动切换时间间隔,3s
+    duration: 1000, //  滑动动画时长1s
   },
 
   onLoad: function () {
     var that = this;
-      wx.setStorageSync("cart", []);
-      //加载菜单
-      req.getRequest('/buyer/product/list',
-          function (res) {
-              let menus = res.data.data;
-              if (!menus) {
-                  menus = [];
+    wx.setStorageSync("cart", []);
+    //加载菜单
+    req.getRequest('/buyer/product/list',
+      function (res) {
+        let menus = res.data.data;
+        if (!menus) {
+          menus = [];
         }
-              that.initProducts(menus, that.data.menuIndex);
-          },
-          function (res) {
-              console.log(res);
+        that.initProducts(menus, that.data.menuIndex);
+      },
+      function (res) {
+        console.log(res);
       }
-      );
+    );
 
     //百度地图
     var BMap = new bmap.BMapWX({
-        ak: 'NEgN0GEeMAmRS01KwFQarCGayZWVrLy7'
+      ak: 'NEgN0GEeMAmRS01KwFQarCGayZWVrLy7'
     });
     var fail = function (data) {
       console.log(data)
@@ -92,53 +98,53 @@ Page({
     });
   },
   onShow: function () {
-      if (this.data.menus) {
-          this.initProducts(this.data.menus, this.data.menuIndex);
-      }
-      //加载购物车数据
-      try {
-          let cart = wx.getStorageSync("cart");
-          this.setData({
-              cart: cart
-          });
-      } catch (e) {
-          console.log(e);
+    if (this.data.menus) {
+      this.initProducts(this.data.menus, this.data.menuIndex);
+    }
+    //加载购物车数据
+    try {
+      let cart = wx.getStorageSync("cart");
+      this.setData({
+        cart: cart
+      });
+    } catch (e) {
+      console.log(e);
     }
   },
-    onHide: function () {
-        wx.setStorageSync("cart", this.data.cart);
-        getApp().globalData.total = this.data.total;
-    },
+  onHide: function () {
+    wx.setStorageSync("cart", this.data.cart);
+    getApp().globalData.total = this.data.total;
+  },
+  //加载商品
+  initProducts: function (menus, index) {
+    console.log(menus);
+    var that = this;
     //加载商品
-    initProducts: function (menus, index) {
-        var that = this;
-        //加载商品
-        console.log("加载商品");
-        let page = menus[index].page;
-        if (!page) {
-            page = 1;
-            menus[index].page = 1;
-        } else {
-            page++;
+    console.log("加载商品");
+    let page = menus[index].page;
+    if (!page) {
+      page = 1;
+      menus[index].page = 1;
+    } else {
+      page++;
+    }
+    req.getRequest('/buyer/product/list/small/prodcut?parentId=' + menus[index].categoryId + '&page=' + page,
+      function (res) {
+        let clildCategories = res.data.data;
+        if (!clildCategories) {
+          clildCategories = [];
         }
-        req.getRequest('/buyer/product/list/small/prodcut?parentId=' + menus[index].categoryId + '&page=' + page,
-            function (res) {
-                let clildCategories = res.data.data;
-                if (!clildCategories) {
-                    clildCategories = [];
-                }
-                console.log(menus);
-                that.initCart(menus, index, clildCategories);
-            },
-            function (res) {
-                console.log(res);
-            }
-        );
+        that.initCart(menus, index, clildCategories);
+      },
+      function (res) {
+        console.log(res);
+      }
+    );
 
-    },
-    //初始化购物车
-    initCart: function (menus, index, clildCategories) {
-        let total = {
+  },
+  //初始化购物车
+  initCart: function (menus, index, clildCategories) {
+    let total = {
       count: 0,
       money: 0.0
     };
@@ -147,216 +153,212 @@ Page({
       var i;
       var j;
       var k;
-
-        clildCategories.forEach(function (categorite, index) {
-            categorite.productInfos.forEach(function (product, i) {
-
-            })
-        })
-        clildCategories.forEach(
-            //小类目-商品数组
-            function (childCategory, index) {
-                childCategory.productInfos.forEach(
+      clildCategories.forEach(
+        //小类目-商品数组
+        function (childCategory, index) {
+          childCategory.productInfos.forEach(
             //商品
-                    function (product, i) {
-                        cart.forEach(
-                            function (cartProduct, j) {
-                                //去除无效数据
-                                if (cartProduct.count == 0) {
-                                    cart.pop(cartProduct);
-                                }
-                                if (cartProduct.productId == product.productId) {
-                                    product.count = cartProduct.count;
-                                    console.log(cartProduct);
-                                    total.count += cartProduct.count;
-                                    total.money = (cartProduct.count * (10 * product.productPrice) + total.money * 10) / 10;
-                                }
-                            }
-                        );
-                    }
-                );
+            function (product, i) {
+              cart.forEach(
+                function (cartProduct, j) {
+                  //去除无效数据
+                  if (cartProduct.count == 0) {
+                    cart.pop(cartProduct);
+                  }
+                  if (cartProduct.productId == product.productId) {
+                    product.count = cartProduct.count;
+                    console.log(cartProduct);
+                    total.count += cartProduct.count;
+                    total.money = (cartProduct.count * (10 * product.productPrice) + total.money * 10) / 10;
+                  }
+                }
+              );
+            }
+          );
         }
-        );
+      );
 
     } else {
       cart = [];
     }
-        // getApp().globalData.total = total;
-        // wx.setStorageSync("cart", cart);
-        menus[index].childCategories = clildCategories;
+    //加载广告数据
+
+    menus[index].childCategories = clildCategories;
+    console.log(menus[index].advertisements);
     this.setData({
-        cart: cart,
-        total: total,
-        menus: menus,
-        smallCategories: clildCategories
+      cart: cart,
+      total: total,
+      menus: menus,
+      smallCategories: clildCategories,
+      advertisements: menus[index].advertisements
     });
   },
   //选择菜单
   selectMenu: function (event) {
     let eventData = event.currentTarget.dataset;
     let menus = this.data.menus;
-      let clildCategories = menus[eventData.idx].clildCategories;
-      if (clildCategories) {
-          this.setData({
-              smallCategories: clildCategories
-          })
-      } else {
-          this.initProducts(menus, eventData.idx);
-      }
+    let clildCategories = menus[eventData.idx].clildCategories;
+    if (clildCategories) {
+      this.setData({
+        smallCategories: clildCategories
+      })
+    } else {
+      this.initProducts(menus, eventData.idx);
+    }
     this.setData({
-        menuIndex: eventData.idx
+      menuIndex: eventData.idx
     });
     // this.data.toView = 'red'
 
   },
   //添加
   addCount: function (event) {
-      //获取目标product
+    //获取目标product
     let eventData = event.currentTarget.dataset;
-      let menus = this.data.menus;
-      let menu = menus.find(function (v) {
+    let menus = this.data.menus;
+    let menu = menus.find(function (v) {
       return v.categoryId == eventData.ctid;
     });
-      let childCategory = menu.childCategories.find(function (v) {
-          return v.categoryId == eventData.sctid;
+    let childCategory = menu.childCategories.find(function (v) {
+      return v.categoryId == eventData.sctid;
     });
 
-      let product = childCategory.productInfos.find(function (v) {
+    let product = childCategory.productInfos.find(function (v) {
       return v.productId == eventData.id;
     });
 
     product.count += 1;
 
-      let total = this.data.total;
+    let total = this.data.total;
     total.count += 1;
     //小数精度问题
-      total.money = (product.productPrice * 10 + total.money * 10) / 10;
+    total.money = (product.productPrice * 10 + total.money * 10) / 10;
     //set数据
     this.setData({
-        total: total,
-        menus: menus,
-        smallCategories: menu.childCategories,
+      total: total,
+      menus: menus,
+      smallCategories: menu.childCategories,
     });
 
     //加入购物车缓存
-      this.cartStore(product);
+    this.cartStore(product);
 
   },
   //移除
   minusCount: function (event) {
-      //获取目标product
+    //获取目标product
     let eventData = event.currentTarget.dataset;
-      let menus = this.data.menus;
-      let menu = menus.find(function (v) {
+    let menus = this.data.menus;
+    let menu = menus.find(function (v) {
       return v.categoryId == eventData.ctid;
     });
-      let childCategory = menu.childCategories.find(function (v) {
-          return v.categoryId == eventData.sctid;
-      });
+    let childCategory = menu.childCategories.find(function (v) {
+      return v.categoryId == eventData.sctid;
+    });
 
-      let product = childCategory.productInfos.find(function (v) {
+    let product = childCategory.productInfos.find(function (v) {
       return v.productId == eventData.id;
-      });
+    });
 
-      //数量为0时不进行操作
+    //数量为0时不进行操作
     if (product.count == 0) {
-        return;
+      return;
     }
 
-      console.log(product);
+    console.log(product);
     product.count -= 1;
 
-      let total = this.data.total;
+    let total = this.data.total;
 
-      total.count -= 1;
+    total.count -= 1;
     total.money = (total.money * 10 - product.productPrice * 10) / 10;
     this.setData({
-        menus: menus,
-        smallCategories: menu.childCategories,
+      menus: menus,
+      smallCategories: menu.childCategories,
     });
 
     getApp().globalData.total = total;
     //移除购物车缓存
-      this.cartStore(product);
+    this.cartStore(product);
 
   },
-    //购物车缓存
-    cartStore: function (product) {
-        try {
-            let cart = this.data.cart;
-            if (!cart) {
-                cart = [];
-            } else {
-                let prod = cart.find(function (v) {
-                    return v.productId == product.productId;
-                });
-                if (prod) {
-                    cart.pop(prod);
-                }
-            }
-            if (product.count >= 1) {
-                cart.push(product);
-            }
-            console.log(cart);
-        } catch (e) {
-            console.log(e);
-        }
-    },
-    //下拉刷新
-    onPullDownRefresh: function () {
-        console.log("下拉");
-        wx.showNavigationBarLoading() //在标题栏中显示加载
-
-        //模拟加载
-        setTimeout(function () {
-            // complete
-            wx.hideNavigationBarLoading() //完成停止加载
-            wx.stopPullDownRefresh() //停止下拉刷新
-        }, 1500);
-    },
-    //上滑加载
-    onReachBottom: function () {
-        var that = this;
-        let menus = that.data.menus;
-        let index = that.data.menuIndex;
-        let page = menus[index].page;
-        if (page == -1) {
-            return;
-        }
-
-        console.log("上滑");
-        that.setData({
-            isHideLoadMore: false
+  //购物车缓存
+  cartStore: function (product) {
+    try {
+      let cart = this.data.cart;
+      if (!cart) {
+        cart = [];
+      } else {
+        let prod = cart.find(function (v) {
+          return v.productId == product.productId;
         });
-        setTimeout(function () {
-            //complete
-            if (page) {
-                page++;
-                req.getRequest('/buyer/product/list/small/prodcut?parentId=' + menus[index].categoryId + '&page=' + page,
-                    function (res) {
-                        let clildCategories = res.data.data;
-                        if (!clildCategories) {
-                            clildCategories = [];
-                        }
-                        if (clildCategories.length == 0) {
-                            menus[index].page = -1;
-                        }
-                        console.log(menus[index].childCategories);
-                        let menusChildCategories = menus[index].childCategories;
-                        menusChildCategories = menusChildCategories.concat(clildCategories);
-                        that.initCart(menus, index, menusChildCategories);
-
-                    },
-                    function (res) {
-                        console.log(res);
-                    }
-                );
-            }
-            that.setData({
-                isHideLoadMore: true
-            });
-        }, 1500);
+        if (prod) {
+          cart.pop(prod);
+        }
+      }
+      if (product.count >= 1) {
+        cart.push(product);
+      }
+      console.log(cart);
+    } catch (e) {
+      console.log(e);
     }
+  },
+  //下拉刷新
+  onPullDownRefresh: function () {
+    console.log("下拉");
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
+  },
+  //上滑加载
+  onReachBottom: function () {
+    var that = this;
+    let menus = that.data.menus;
+    let index = that.data.menuIndex;
+    let page = menus[index].page;
+    if (page == -1) {
+      return;
+    }
+
+    console.log("上滑");
+    that.setData({
+      isHideLoadMore: false
+    });
+    setTimeout(function () {
+      //complete
+      if (page) {
+        page++;
+        req.getRequest('/buyer/product/list/small/prodcut?parentId=' + menus[index].categoryId + '&page=' + page,
+          function (res) {
+            let clildCategories = res.data.data;
+            if (!clildCategories) {
+              clildCategories = [];
+            }
+            if (clildCategories.length == 0) {
+              menus[index].page = -1;
+            }
+            console.log(menus[index].childCategories);
+            let menusChildCategories = menus[index].childCategories;
+            menusChildCategories = menusChildCategories.concat(clildCategories);
+            that.initCart(menus, index, menusChildCategories);
+
+          },
+          function (res) {
+            console.log(res);
+          }
+        );
+      }
+      that.setData({
+        isHideLoadMore: true
+      });
+    }, 1500);
+  }
 
 
 });
--- utils/request.js
+++ utils/request.js
@@ -3,21 +3,18 @@ function postRequest() {
 }
 
 function getRequest(url, success, fail) {
-    console.log("调用公共方法：getRequest");
-    wx.request({
-        url: getApp().globalData.serviceUrl + url,
-        success: res = > {
-        success(res);
-},
-    fail: res =
->
-    {
-        fail(res);
+  console.log("调用公共方法：getRequest");
+  wx.request({
+    url: getApp().globalData.serviceUrl + url,
+    success: res =>{
+      success(res);
+    },
+    fail: res =>{
+      fail(res);
     }
-})
-    ;
+  });
 }
 
 module.exports = {
-    getRequest: getRequest
+  getRequest: getRequest
 }
