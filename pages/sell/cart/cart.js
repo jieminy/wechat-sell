c574b08b008e6ae242016ad53c9102705a93e9ff
jieminy <jiemin.y@163.com>
2018-05-15 00:15:08 +0800

    -购物车总价bug修改

--- pages/sell/cart/cart.js
+++ pages/sell/cart/cart.js
@@ -15,7 +15,7 @@ Page({
       if (cart) {
         cart.forEach(function (product, idx) {
           total.count += product.count;
-          if (product.activity.atvId == 1){
+          if (product.activity!=null && product.activity.atvId == 1){
             total.money = (total.money * 10000 + product.count * (product.productPrice * 10 * 10)  * (product.activity.discount * 10 * 10))/10000 ;
           }else{
             total.money = (total.money * 100 + product.count * product.productPrice * 100) / 100;
@@ -49,7 +49,7 @@ Page({
       product.count -= 1;
       let total = this.data.total;
       total.count -= 1;
-      if (product.activity.atvId == 1) {
+      if (product.activity != null && product.activity.atvId == 1) {
         total.money = (total.money * 10000 - (product.productPrice * 10 * 10) * (product.activity.discount * 10 * 10)) / 10000;
         }else{
       total.money = (total.money * 10 * 10 - product.productPrice * 10 * 10) / 100;
